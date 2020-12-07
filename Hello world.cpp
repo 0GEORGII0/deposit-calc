3d12d3afeaebdab14f7fe7ea964c40f584a2b4ef
0GEORGII0 <zherdev.georgii@gmail.com>
2020-12-07 17:01:51 +0700

    Implement input-output functions

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	cout << "hello world" << endl;
+	int d, x, s;
+
+
+	cout << "Na skolko dnei vklad" << endl;
+	cin >> d;
+	
+	cout << "Summa" << endl;
+	cin >> x;
 	
 	return 0;
 }
